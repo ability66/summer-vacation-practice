cb5a3eb50bb6f745e63014ee3c5ae1e173acbfe5
gilGAMEsh002 <2872752003@qq.com>
2023-07-09 13:52:31 +0800

    删除文件夹里后端的东西,现在只保留了前端的文件

--- SummerVacationPractice/src/main/java/com/system/summervacationpractice/mapper/ClassMapper.java
+++
@@ -1,7 +0,0 @@
-package com.system.summervacationpractice.mapper;
-
-import java.util.List;
-
-public interface ClassMapper {
-
-}
--- SummerVacationPractice/src/main/java/com/system/summervacationpractice/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.system.summervacationpractice.mapper;
-
-import com.system.summervacationpractice.entity.User;
-
-import java.util.List;
-
-public interface UserMapper {
-    List<User> selectAll();//1
-}
